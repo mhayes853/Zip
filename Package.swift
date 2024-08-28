949d305ee50236338d5d7268b3f9bab22fa14839
mhayes853 <matthewha853@gmail.com>
2024-08-28 13:06:44 -0700

    swift format

--- Package.swift
+++ Package.swift
@@ -3,27 +3,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "Zip",
-    products: [
-        .library(name: "Zip", targets: ["Zip"])
-    ],
-    targets: [
-        .target(
-            name: "Minizip",
-            dependencies: [],
-            path: "Zip/minizip",
-            exclude: ["module"],
-            linkerSettings: [
-                .linkedLibrary("z")
-            ]),
-        .target(
-            name: "Zip",
-            dependencies: ["Minizip"],
-            path: "Zip",
-            exclude: ["minizip", "zlib"]),
-        .testTarget(
-            name: "ZipTests",
-            dependencies: ["Zip"],
-            path: "ZipTests"),
-    ]
+  name: "Zip",
+  products: [
+    .library(name: "Zip", targets: ["Zip"])
+  ],
+  targets: [
+    .target(
+      name: "Minizip",
+      dependencies: [],
+      path: "Zip/minizip",
+      exclude: ["module"],
+      linkerSettings: [
+        .linkedLibrary("z")
+      ]
+    ),
+    .target(
+      name: "Zip",
+      dependencies: ["Minizip"],
+      path: "Zip",
+      exclude: ["minizip", "zlib"]
+    ),
+    .testTarget(
+      name: "ZipTests",
+      dependencies: ["Zip"],
+      path: "ZipTests"
+    )
+  ]
 )
--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -3,28 +3,32 @@
 import PackageDescription
 
 let package = Package(
-    name: "Zip",
-    products: [
-        .library(name: "Zip", targets: ["Zip"])
-    ],
-    targets: [
-        .systemLibrary(
-            name: "CZlib",
-            path: "Zip/zlib",
-            pkgConfig: "zlib"),
-        .target(
-            name: "Minizip",
-            dependencies: ["CZlib"],
-            path: "Zip/minizip",
-            exclude: ["module"]),
-        .target(
-            name: "Zip",
-            dependencies: ["Minizip"],
-            path: "Zip",
-            exclude: ["minizip", "zlib"]),
-        .testTarget(
-            name: "ZipTests",
-            dependencies: ["Zip"],
-            path: "ZipTests"),
-    ]
+  name: "Zip",
+  products: [
+    .library(name: "Zip", targets: ["Zip"])
+  ],
+  targets: [
+    .systemLibrary(
+      name: "CZlib",
+      path: "Zip/zlib",
+      pkgConfig: "zlib"
+    ),
+    .target(
+      name: "Minizip",
+      dependencies: ["CZlib"],
+      path: "Zip/minizip",
+      exclude: ["module"]
+    ),
+    .target(
+      name: "Zip",
+      dependencies: ["Minizip"],
+      path: "Zip",
+      exclude: ["minizip", "zlib"]
+    ),
+    .testTarget(
+      name: "ZipTests",
+      dependencies: ["Zip"],
+      path: "ZipTests"
+    )
+  ]
 )
